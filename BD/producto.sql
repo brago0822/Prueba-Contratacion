9ee460e06ab0e9a1743185b7b487717ea3618406
Brayan Esguerra <brago0822@gmail.com>
2019-05-01 23:21:13 -0500

    Se agregan líneas de creación de secuencia y otorgar todos los privilegios al usuario creado

--- BD/producto.sql
+++ BD/producto.sql
@@ -20,3 +20,8 @@ GRANT INSERT ON prueba_empresa_db.producto TO examen_user;
 GRANT SELECT ON prueba_empresa_db.producto TO examen_user;
 GRANT UPDATE ON prueba_empresa_db.producto TO examen_user;
 GRANT DELETE ON prueba_empresa_db.producto TO examen_user;
+
+create sequence prueba_empresa_db.sec_producto start 1 increment 1
+	GRANT ALL PRIVILEGES ON prueba_empresa_db.sec_producto TO examen_user;
+
+GRANT ALL PRIVILEGES ON prueba_empresa_db.prueba_contratacion to examen_user; 
